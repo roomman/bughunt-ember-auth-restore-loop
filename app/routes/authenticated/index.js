a9861135852da22a5bb4485424d756723358dc4b
Simon Zimmerman <simon@mysolomon.co.uk>
2022-11-30 21:19:57 +0000

    chore: added a user to test data

--- app/routes/authenticated/index.js
+++ app/routes/authenticated/index.js
@@ -4,7 +4,13 @@ import { service } from '@ember/service';
 export default class AuthenticatedIndexRoute extends Route {
   @service session;
 
+  @service store;
+
   beforeModel(transition) {
     this.session.requireAuthentication(transition, 'login');
+  }
+
+  model() {
+    return this.store.findAll('user');
   }
 }
